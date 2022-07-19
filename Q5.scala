309326b4f266fbba4093f2d50813cc36ba508be5
umasha00 <109022189+umasha00@users.noreply.github.com>
2022-07-19 09:39:44 +0530

    Update Q5.scala

--- Q5.scala
+++ Q5.scala
@@ -1,21 +1,27 @@
-def profit( ticketPrice: Int, attendance: Int) : Int = { return (attendance*ticketPrice) - ( 500 + 3*attendance); };
 
-var decPrice = 5;
-var decAttendance = 20;
-println("Enter the normal ticket price:");
-val ticketPrice : Int = scala.io.StdIn.readInt();
-println("Enter the attendance:");
-val attendence : Int = scala.io.StdIn.readInt();
+object q4 {
+def main(args: Array[String]): Unit = {
+	     
+       
+ println(profit(5));
+ println(profit(10));
+ println(profit(15));
+ println(profit(20));
+ println(profit(25));
+ println(profit(30));
+ println(profit(35));
+ println(profit(40));
 
-var a = profit(ticketPrice, attendence); 
-var b = profit(ticketPrice+decPrice,attendence -decAttendance);
-var c = profit(ticketPrice-decPrice, attendence +decAttendance);
+}
 
-var bestProfit = if ( a>b && a>c) a else if ( b>a && b>c ) b else c;
-
-if( bestProfit ==a )
-    println("The best ticket price is Rs:"+ ticketPrice + "\n\n");
-else if ( bestProfit ==b)
-    println("The best ticket price is Rs:"+ (ticketPrice + decPrice)+ "\n\n");
-else
-    println("The best ticket price is Rs:"+ (ticketPrice - decPrice) + "\n\n" );
+ def attendees(price:Int):Int=120+(15-price)/5*20 ;
+	     
+ def revenue(price:Int):Int = attendees(price)*price ;
+      
+ def cost(price:Int):Int=500+attendees(price) ;
+      
+ def profit(price:Int):Int =revenue(price)- cost(price) ;
+           
+     
+}
+       
